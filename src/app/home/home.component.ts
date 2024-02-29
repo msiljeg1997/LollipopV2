20a6e782c4c178f1b778035f86859691a85bd2a8
Matija <95matija@gmail.com>
2024-02-29 12:27:54 +0100

    dodan_js_za_DOM_metlica_Cistaljka

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    const width = event.target.innerWidth;
+    const rightImage = this.el.nativeElement.querySelector('.right-image');
+  
+    if (rightImage) {
+      if (width < 770) {
+        this.renderer.setStyle(rightImage, 'display', 'none');
+      } else if (width <= 995 && width >= 766) {
+        this.renderer.setStyle(rightImage, 'display', 'block');
+        this.renderer.setStyle(rightImage, 'right', '-35%');
+      } else if (width <= 1200 && width > 920) {
+        this.renderer.setStyle(rightImage, 'display', 'block');
+        this.renderer.setStyle(rightImage, 'right', '-50%');
+      } else {
+        this.renderer.setStyle(rightImage, 'display', 'block');
+        this.renderer.setStyle(rightImage, 'right', '-30%');
+      }
+    }
+  }
 }
