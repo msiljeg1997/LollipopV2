c21911705243ef9f55ac47caad5379e4189cb160
Matija <95matija@gmail.com>
2024-02-29 23:00:57 +0100

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { GalleryComponent } from './gallery/gallery.component';
@@ -12,7 +10,6 @@ import { EquipmentComponent } from './equipment/equipment.component';
 import { FooterComponent } from './footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ServicesComponent } from './services/services.component';
-import { CarouselNavigationComponent } from './carousel-navigation/carousel-navigation.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgIf } from '@angular/common';
@@ -29,7 +26,6 @@ import { ReactiveFormsModule } from '@angular/forms';
     EquipmentComponent,
     FooterComponent,
     ServicesComponent,
-    CarouselNavigationComponent,
     CarouselComponent,
   ],
   imports: [
