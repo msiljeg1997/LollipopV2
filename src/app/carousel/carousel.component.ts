0d5e1b9673e162a756a512bc7ae22d08bca920e5
Matija <95matija@gmail.com>
2024-03-11 10:04:54 +0100

    zadnji update prije SEO

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -10,10 +10,29 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   encapsulation: ViewEncapsulation.None,
 })
 export class CarouselComponent {
-  constructor(private modalService: NgbModal) {}
-
-  images = [1, 2, 3].map((n) => `/assets/slika${n}.jpeg`);
+  constructor(private modalService: NgbModal) { }
   showNavigationIndicators = true;
   showNavigationArrows = true;
   selectedImage: string = '';
+  images = [
+    { type: 'video', src: "/assets/gal1.mp4" },
+    { type: 'image', src: '/assets/gal2.jpeg' },
+    { type: 'image', src: '/assets/gal3.jpeg' },
+    { type: 'image', src: '/assets/gal4.jpeg' },
+    { type: 'image', src: '/assets/gal5.jpeg' },
+    { type: 'image', src: '/assets/gal6.jpeg' },
+    { type: 'image', src: '/assets/gal7.jpeg' },
+    { type: 'image', src: '/assets/gal8.jpeg' },
+    { type: 'image', src: '/assets/gal9.jpeg' },
+    { type: 'image', src: '/assets/gal10.jpeg' },
+    { type: 'image', src: '/assets/gal11.jpeg' },
+    { type: 'video', src: '/assets/gal12.mp4' },
+    { type: 'image', src: '/assets/gal13.jpeg' },
+    { type: 'image', src: '/assets/gal14.jpeg' },
+    { type: 'video', src: '/assets/gal15.mp4' },
+    { type: 'video', src: '/assets/gal16.mp4' },
+    { type: 'video', src: '/assets/gal17.mp4' },
+    { type: 'image', src: '/assets/gal18.jpeg' },
+    // ...
+  ];
 }
